e59519688ad6f8e6c49bbb864b704c085e5f7188
fatihilhan42 <63750425+fatihilhan42@users.noreply.github.com>
2021-05-16 11:26:51 +0300

    Add files via upload

---
+++ multiplexer/testbench.vhd
@@ -0,0 +1,114 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   11:21:41 05/16/2021
+-- Design Name:   
+-- Module Name:   D:/programss/GITHUB VHDL/projeler/mux/multiplexer/testbench.vhd
+-- Project Name:  multiplexer
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: mux_vhdl
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY testbench IS
+END testbench;
+ 
+ARCHITECTURE behavior OF testbench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT mux_vhdl
+    PORT(
+         A : IN  std_logic;
+         B : IN  std_logic;
+         C : IN  std_logic;
+         D : IN  std_logic;
+         S0 : IN  std_logic;
+         S1 : IN  std_logic;
+         Z : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic := '0';
+   signal B : std_logic := '0';
+   signal C : std_logic := '0';
+   signal D : std_logic := '0';
+   signal S0 : std_logic := '0';
+   signal S1 : std_logic := '0';
+
+ 	--Outputs
+   signal Z : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: mux_vhdl PORT MAP (
+          A => A,
+          B => B,
+          C => C,
+          D => D,
+          S0 => S0,
+          S1 => S1,
+          Z => Z
+        );
+
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+	 A <= '1';
+    B <= '0';
+    C <= '1';
+    D <= '0';       
+ 
+    S0 <= '0'; S1 <= '0';
+ 
+      wait for 100 ns; 
+ 
+    S0 <= '1'; S1 <= '0';
+ 
+      wait for 100 ns; 
+ 
+    S0 <= '0'; S1 <= '1';
+ 
+        wait for 100 ns;   
+ 
+    S0 <= '0'; S1 <= '1';  
+ 
+        wait for 100 ns;   
+ 
+    end process;
+      -- insert stimulus here 
+
+      
+
+END;
